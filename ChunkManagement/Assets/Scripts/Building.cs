7b4515d251fec3cdf9cea67bc6baef5640a0795b
Az <thomas.guyamier@gmail.com>
2020-12-14 11:59:42 +0100

    ParserGML Import

---
+++ ChunkManagement/Assets/Scripts/Building.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Building
+{
+    public List<Vector3> points;
+
+    public Building(List<Vector3> pts)
+    {
+        points = new List<Vector3>(pts);
+    }
+}
---
+++ ChunkManagement/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    public string glmPath;
+    public List<Building> city;
+
+
+    private void Awake()
+    {
+        if(glmPath != "")
+        {
+            city = ParserGML.GetBuildingsFromGML(glmPath);
+        }
+        else
+        {
+            Debug.Log("GlmPath Empty");
+        }
+    }
+}
